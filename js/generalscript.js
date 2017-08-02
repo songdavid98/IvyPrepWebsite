8886bf07b116bad8e12c6c515f729e38da70f92e
songdavid98 <songdavid98@gmail.com>
2017-08-02 16:29:38 -0400

    hunter.html: fixed the resizing issue in Hunter.html

---
+++ js/generalscript.js
@@ -0,0 +1,8 @@
+//    This sets an href to the element
+//    for onclicks
+//    Parameters: String location.
+//    Return: none.
+function locationLink(locationstring) {
+    "use strict";
+    location.href = locationstring;
+}
